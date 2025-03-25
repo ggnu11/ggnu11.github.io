8070dd31247fcd38d9330790460e9b8f822b811d
choi0712 <130032473+ggnu11@users.noreply.github.com>
2025-03-25 10:11:35 +0900

    [Header] 타이핑 추가 및 문구 수정

---
+++ js/typed.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed("#typed", {
+    strings: [
+      "안녕하세요!",
+      "프론트엔드 개발자 최영훈입니다.",
+      "새로운 기술을 배우고 성장하는 것을 즐깁니다.",
+    ],
+    typeSpeed: 50,
+    backSpeed: 35,
+    loop: true,
+  });
+});
